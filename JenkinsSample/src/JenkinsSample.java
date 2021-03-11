d79f1d3aea26905410ef77fbc2ac5a96a6cdff67
Kavi96 <79734491+Kavi96@users.noreply.github.com>
2021-03-11 09:27:40 +0530

    Jenkins

--- JenkinsSample/src/JenkinsSample.java
+++ JenkinsSample/src/JenkinsSample.java
@@ -4,6 +4,7 @@ public class JenkinsSample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("JenkinsSample");
+		System.out.println("Pushed");
 	}
 
 }
